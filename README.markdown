2dea5d6469ab6f4a7ea8cb713c3fb995a8978817
manuel muñoz solera <mamusino@gmail.com>
2009-01-20 23:40:21 +0100

    base reticulator

--- README.markdown
+++ README.markdown
@@ -1,7 +1,7 @@
 jQuery Reticulator
 ==================
 
-Reticulator takes its name from the fancy [Fireworks extenison](http://sofanaranja.com/reticulator/) [(*)]() created by [Ale Muñoz](http://bomberstudios.com/). Now this extension is part of [OrangeCommands](http://bomberstudios.com/orangecommands).
+Reticulator takes its name from the fancy [Fireworks extenison](http://sofanaranja.com/reticulator/) created by [Ale Muñoz](http://bomberstudios.com/). Now this extension is part of [OrangeCommands](http://bomberstudios.com/orangecommands).
 
 I like the idea of the CSS frameworks like [YUI](http://developer.yahoo.com/yui/grids/), [blueprint](http://www.blueprintcss.org/), [960](http://960.gs/), the über complete [compass-style](http://compass-style.org/)... but in my real work I don't use this tools. With this plugin you can define your regular grid and use it like the Fireworks guides.
 
--- jquery.reticulator.js
+++ jquery.reticulator.js
@@ -8,19 +8,79 @@
 * http://www.opensource.org/licenses/mit-license.php
 * http://www.gnu.org/licenses/gpl.html
 */
- 
+
 /*
 * @description Plugin that overlays a defined grid to follow for layout purposes
 *
 */
- 
+
 (function($) {
     // configuration
     $.reticulator = function(options) {
+        var defaults = {
+            layoutWidth: 951,
+            layoutColumns: 16,
+            gutterWidth: 9,
+            gridAlign: "center",
+            guideColor: "#00FF00",
+            guideOpacity: 0.8
+        };
         
-      }
+        var options = $.extend(defaults, options);
+
+        var reticulator = {
+            gridCont: '',
+            gridCount: 0,
+            gridCols: ( options.layoutWidth - (options.layoutColumns * options.gutterWidth) ) / options.layoutColumns
+        }
+        reticulator.calculateGrid = function(){
+            // grid container
+            reticulator.gridCont = document.createElement("div");
+            reticulator.gridCont.setAttribute('id', 'reticulator_grid_container');
+            $(reticulator.gridCont).css({
+                width: $(document).width() + "px",
+                position: "fixed",
+                textAlign: options.gridAlign,
+                opacity: options.guideOpacity,
+                top: 0
+            });
+            
+            var gridLayout = document.createElement("div");
+            $(gridLayout).css({
+                width: options.layoutWidth + "px",
+                margin: "0px auto"
+            })
+            $(reticulator.gridCont).append(gridLayout);
+            
+            for (var i = 0; i < options.layoutColumns * 2; i++) {
+                var gridGuide = document.createElement("div");
+                $(gridGuide).css({
+                    height: $(document).height() + "px",
+                    borderLeft: "1px solid " + options.guideColor,
+                    marginLeft: reticulator.gridCount + "px"
+                });
+                
+                if(i%2 == 0) reticulator.gridCount = reticulator.gridCount + reticulator.gridCols
+                else reticulator.gridCount = reticulator.gridCount + reticulator.gutterWidth
+                
+                console.log(reticulator.gridCols + reticulator.gridCount)
+                $(gridLayout).append(gridGuide);
+            }
+            
+            $(document.body).prepend(reticulator.gridCont);
+
+            // grid container
+        };
+        
+        // resize the grid container
+        reticulator.resizeGridCont = function() {
+            $(reticulator.gridCont).width( $(document).width() + "px" );
+        }
+        $(window).resize(function(){ reticulator.resizeGridCont(); });
+        
+        // 
+        reticulator.calculateGrid();
+    };
+    
 })(jQuery);
- 
-jQuery(document).ready(function($) {
- 
-});
+
