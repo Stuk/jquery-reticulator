e84a3e748ab0490610b45246a01140443e7f3837
manuel muñoz solera <mamusino@gmail.com>
2009-01-20 22:10:25 +0100

    first commit

---
+++ jquery.reticulator.js
@@ -0,0 +1,26 @@
+/* jQuery Reticulator
+* Version 0.01
+* Written by Manuel Muñoz Solera (mamuso@mamuso.net)
+* @requires jQuery v1.2
+*
+* Copyright 2009 Manuel Muñoz Solera
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+*/
+ 
+/*
+* @description Plugin that overlays a defined grid to follow for layout purposes
+*
+*/
+ 
+(function($) {
+    // configuration
+    $.reticulator = function(options) {
+        
+      }
+})(jQuery);
+ 
+jQuery(document).ready(function($) {
+ 
+});
